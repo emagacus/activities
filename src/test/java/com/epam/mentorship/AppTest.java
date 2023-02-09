95e0b78afb7a6233cf94f89b13f4af7ec1257ca2
emagacus <emagaca@hotmail.com>
2023-02-09 13:45:53 -0600

    first commit

---
+++ src/test/java/com/epam/mentorship/AppTest.java
@@ -0,0 +1,18 @@
+package com.epam.mentorship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
