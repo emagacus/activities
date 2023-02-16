b34f4174f761e91e00a2c367af8287b010eb3712
emagacus <emagaca@hotmail.com>
2023-02-16 16:35:26 -0600

    Loop exercise 1

---
+++ src/main/java/core/loopActivities/RepeatEnd.java
@@ -0,0 +1,22 @@
+package core.loopActivities;
+
+public class RepeatEnd {
+    /**
+     *
+     Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive
+     repeatEnd("Hello", 3) → "llollollo"
+     repeatEnd("Hello", 2) → "lolo"
+     repeatEnd("Hello", 1) → "o"
+     * @param str
+     * @param n
+     * @return
+     */
+    public String repeatEnd(String str, int n) {
+        String sub = str.substring(str.length() - n);
+        String result = sub;
+        for(int i = 1; i < n; i++){
+            result += sub;
+        }
+        return result;
+    }
+}
